6ced46c2921979c69836e5a615fd907444c46ff9
Dinataaa <86862722+Dinataaa@users.noreply.github.com>
2021-11-13 20:23:53 +0700

    Update fazd.js

--- connect/fazd.js
+++ connect/fazd.js
@@ -655,92 +655,68 @@ Current Prefix : ${multi ? "•π÷×¶∆£¢€¥®™✓_=|~!?#$%^&.+-," : no
             }
         }
         break
-
-        case prefix + 'stickerwm':
-        case prefix + 'swm':
-        case prefix + 'take':
-        case prefix + 'takesticker':
-        case prefix + 'takestick': {
-            if (args.length < 2) return reply(`Penggunaan ${command} nama|author`)
-            let packname1 = q.split('|')[0] ? q.split('|')[0] : q
-            let author1 = q.split('|')[1] ? q.split('|')[1] : ''
-            if (isImage || isQuotedImage) {
-                let encmedia = isQuotedImage ? JSON.parse(JSON.stringify(xfazd).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : xfazd
-                let media = await fazd.downloadAndSaveMediaMessage(encmedia, `./sticker/${sender}`)
-                exif.create(packname1, author1, `stickwm_${sender}`)
-                await ffmpeg(`${media}`)
-                    .input(media)
-                    .on('start', function(cmd) {
-                        console.log(`Started : ${cmd}`)
-                    })
-                    .on('error', function(err) {
-                        console.log(`Error : ${err}`)
-                        fs.unlinkSync(media)
-                        reply(mess.error.api)
-                    })
-                    .on('end', function() {
-                        console.log('Finish')
-                        exec(`webpmux -set exif ./sticker/stickwm_${sender}.exif ./sticker/${sender}.webp -o ./sticker/${sender}.webp`, async (error) => {
-                            if (error) return reply(mess.error.api)
-                            fazd.sendMessage(from, fs.readFileSync(`./sticker/${sender}.webp`), sticker, {
-                                quoted: xfazd
-                            })
-                            fs.unlinkSync(media)
-                            fs.unlinkSync(`./sticker/${sender}.webp`)
-                            fs.unlinkSync(`./sticker/stickwm_${sender}.exif`)
-                        })
-                    })
-                    .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-                    .toFormat('webp')
-                    .save(`./sticker/${sender}.webp`)
-            } else if ((isVideo && xfazd.message.videoMessage.fileLength < 10000000 || isQuotedVideo && xfazd.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.fileLength < 10000000)) {
-                let encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(xfazd).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : xfazd
-                let media = await fazd.downloadAndSaveMediaMessage(encmedia, `./sticker/${sender}`)
-                exif.create(packname1, author1, `stickwm_${sender}`)
-                reply(mess.wait)
-                await ffmpeg(`${media}`)
-                    .inputFormat(media.split('.')[4])
-                    .on('start', function(cmd) {
-                        console.log(`Started : ${cmd}`)
-                    })
-                    .on('error', function(err) {
-                        console.log(`Error : ${err}`)
-                        fs.unlinkSync(media)
-                        let tipe = media.endsWith('.mp4') ? 'video' : 'gif'
-                        reply(mess.error.api)
-                    })
-                    .on('end', function() {
-                        console.log('Finish')
-                        exec(`webpmux -set exif ./sticker/stickwm_${sender}.exif ./sticker/${sender}.webp -o ./sticker/${sender}.webp`, async (error) => {
-                            if (error) return reply(mess.error.api)
-                            fazd.sendMessage(from, fs.readFileSync(`./sticker/${sender}.webp`), sticker, {
-                                quoted: xfazd
-                            })
-                            fs.unlinkSync(media)
-                            fs.unlinkSync(`./sticker/${sender}.webp`)
-                            fs.unlinkSync(`./sticker/stickwm_${sender}.exif`)
-                        })
-                    })
-                    .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-                    .toFormat('webp')
-                    .save(`./sticker/${sender}.webp`)
-            } else if (isQuotedSticker) {
-                let encmedia = JSON.parse(JSON.stringify(xfazd).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo
-                let media = await fazd.downloadAndSaveMediaMessage(encmedia, `./sticker/${sender}`)
-                exif.create(packname1, author1, `takestick_${sender}`)
-                exec(`webpmux -set exif ./sticker/takestick_${sender}.exif ./sticker/${sender}.webp -o ./sticker/${sender}.webp`, async (error) => {
-                    if (error) return reply(mess.error.api)
-                    fazd.sendMessage(from, fs.readFileSync(`./sticker/${sender}.webp`), sticker, {
-                        quoted: xfazd
-                    })
-                    fs.unlinkSync(media)
-                    fs.unlinkSync(`./sticker/takestick_${sender}.exif`)
-                })
-            } else {
-                reply(`Kirim gambar/video dengan caption ${prefix}stickerwm nama|author atau tag gambar/video yang sudah dikirim\nNote : Durasi video maximal 10 detik`)
+case prefix+'sticker':
+            case prefix+'stiker':
+            case prefix+'s':
+            case prefix+'stickergif':
+            case prefix+'sgif':{
+                if (isImage || isQuotedImage) {
+                    let encmedia = isQuotedImage ? JSON.parse(JSON.stringify(xfazd).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : xfazd
+                    let media = await fazd.downloadAndSaveMediaMessage(encmedia, `./sticker/${sender}`)
+                    await ffmpeg(`${media}`)
+							.input(media)
+							.on('start', function (cmd) {
+								console.log(`Started : ${cmd}`)
+							})
+							.on('error', function (err) {
+								console.log(`Error : ${err}`)
+								fs.unlinkSync(media)
+								reply(mess.error.api)
+							})
+							.on('end', function () {
+								console.log('Finish')
+								exec(`webpmux -set exif ./sticker/data.exif ./sticker/${sender}.webp -o ./sticker/${sender}.webp`, async (error) => {
+                  if (error) return reply(mess.error.api)
+									fazd.sendMessage(from, fs.readFileSync(`./sticker/${sender}.webp`), sticker, {quoted: xfazd})
+                  fs.unlinkSync(media)	
+									fs.unlinkSync(`./sticker/${sender}.webp`)	
+								})
+							})
+							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+							.toFormat('webp')
+							.save(`./sticker/${sender}.webp`)
+                } else if ((isVideo && xfazd.message.videoMessage.fileLength < 10000000 || isQuotedVideo && xfazd.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.fileLength < 10000000)) {
+                    let encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(xfazd).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : xfazd
+                    let media = await fazd.downloadAndSaveMediaMessage(encmedia, `./sticker/${sender}`)
+				           	reply(mess.wait)
+                    await ffmpeg(`${media}`)
+							      .inputFormat(media.split('.')[4])
+							.on('start', function (cmd) {
+								console.log(`Started : ${cmd}`)
+							})
+							.on('error', function (err) {
+								console.log(`Error : ${err}`)
+								fs.unlinkSync(media)
+								let tipe = media.endsWith('.mp4') ? 'video' : 'gif'
+								reply(mess.error.api)
+							})
+							.on('end', function () {
+								console.log('Finish')
+								exec(`webpmux -set exif ./sticker/data.exif ./sticker/${sender}.webp -o ./sticker/${sender}.webp`, async (error) => {
+									if (error) return reply(mess.error.api)
+									fazd.sendMessage(from, fs.readFileSync(`./sticker/${sender}.webp`), sticker, {quoted: xfazd})
+                  fs.unlinkSync(media)
+									fs.unlinkSync(`./sticker/${sender}.webp`)
+								})
+							})
+							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+							.toFormat('webp')
+							.save(`./sticker/${sender}.webp`)
+                } else {
+                    reply(`Kirim gambar/video dengan caption ${prefix}sticker atau tag gambar/video yang sudah dikirim\nNote : Durasi video maximal 10 detik`)
+                }
             }
-        }
-        break
+                break
 
         case prefix + 'sc':
         case prefix + 'sourcecode': {
